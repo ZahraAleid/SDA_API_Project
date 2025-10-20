66685cd6b361e964683cfaf5a5be80d00f7afb5c
zahra <zahra.eid112@gmail.com>
2025-10-20 13:50:03 +0300

    20/10/2025

---
+++ src/test/java/pojos/BookingResponsePojo.java
@@ -0,0 +1,26 @@
+package pojos;
+
+public class BookingResponsePojo {
+
+    private Integer bookingid;
+    private   BookingPojo booking;
+
+
+    public Integer getBookingid() {
+        return bookingid;
+    }
+
+
+    public BookingPojo getBooking() {
+        return booking;
+    }
+
+
+    @Override
+    public String toString() {
+        return "BookingResponsePojo{" +
+                "bookingid=" + bookingid +
+                ", booking=" + booking +
+                '}';
+    }
+}
